17d57bb8ebe365ae14b08afae2a92772155fedb0
K S Akkash <s.akkash@gmail.com>
2024-10-06 15:28:53 +0530

    updated middlware

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes:['/','/view-listing/:path*']
+  publicRoutes:['/','/commercial/:path*']
 });
 
 export const config = {
