1f90811a9b6b9b6d00a60ef4a4ce16aa3ef2d95f
K S Akkash <s.akkash@gmail.com>
2024-10-06 20:39:08 +0530

    Updated layout

--- app/layout.js
+++ app/layout.js
@@ -15,9 +15,6 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
     <html lang="en">
-    <Head>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
       <body className={inter.className}>
         <Provider>
          <Toaster />
