f2ec718dcd0e794285d529c275704ea942070f55
K S Akkash <s.akkash@gmail.com>
2024-11-10 09:52:53 +0530

    robots

--- app/robots.js
+++ app/robots.js
@@ -1,1 +1,14 @@
- 
+export default async function robots() {
+  const baseUrl = process.env.SITE_URL || 'https://vabgo.com'
+  
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin/', '/api/'],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+} 
